e5cdab89ca6f63260e1c2e508e8f6d4086435042
tarekguelma013-lgtm <tarekguelma013@gmail.com>
2026-09-07 12:03:40 +0100

    Add translations for Arabic, French, and English

---
+++ translations.js
@@ -0,0 +1,23 @@
+const translations = {
+  ar: {
+    login_title: "تسجيل الدخول",
+    email_label: "البريد الإلكتروني",
+    password_label: "كلمة السر",
+    login_button: "دخول",
+    dir: "rtl"
+  },
+  fr: {
+    login_title: "Connexion",
+    email_label: "E-mail",
+    password_label: "Mot de passe",
+    login_button: "Connexion",
+    dir: "ltr"
+  },
+  en: {
+    login_title: "Login",
+    email_label: "Email",
+    password_label: "Password",
+    login_button: "Login",
+    dir: "ltr"
+  }
+};
